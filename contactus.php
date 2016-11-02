8959a8e6c4a0d28a6dfe1112ac03a2d0c8d021e7
Mansura Chowdhury <c.mansura@gmail.com>
2016-11-02 10:09:12 -0400

    added workshop info

--- contactus.php
+++ contactus.php
@@ -195,7 +195,7 @@ mail($my_email,$subject,$message,$headers);
 </div>
         <div class="bg">
           <h1>2<sup>nd</sup> International Conference on Nanomedicine,<br>Drug Delivery, and Tissue Engineering (NDDTE'17)</h1>
-          <p class="subhead">April 5 - 6, 2017 | Barcelona, Spain</p>
+          <p class="subhead">April 4 - 6, 2017 | Barcelona, Spain</p>
 
           <a href="../papers" class="bg-link">Paper Submission</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
@@ -223,7 +223,7 @@ mail($my_email,$subject,$message,$headers);
 
         <div class="bg">
           <h1>2<sup>nd</sup> International Conference on Nanomedicine,<br>Drug Delivery, and Tissue Engineering (NDDTE'17)</h1>
-          <p class="subhead">April 5 - 6, 2017 | Barcelona, Spain</p>
+          <p class="subhead">April 4 - 6, 2017 | Barcelona, Spain</p>
 
           <a href="../papers" class="bg-link">Paper Submission</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
@@ -253,7 +253,19 @@ mail($my_email,$subject,$message,$headers);
     <div>
       <h2 class="title">1</h2>
       <p class="bold">NDDTE 2017:</p>
-      <p class="body">NDDTE 2017 will  be held in Barcelona, Spain on April 5 - 6, 2017 at the Alimara Hotel Barcelona.</p>
+      <p class="body">NDDTE 2017 will  be held in Barcelona, Spain on April 4 - 6, 2017 at the Alimara Hotel Barcelona.</p>
+
+      <p class="bold">RAN'17 Workshop</p>
+      <p class="body">As per popular request, the organizing committee has decided to extend the RAN'17 congress to three days (now April 4 - 6, 2017). The new high-level schedule is as follows:</p>
+
+      <ul>
+        <li>Day 1: Workshop(s) and Registration</li>
+        <li>Day 2: Main Track Conference Sessions</li>
+        <li>Day 2: Main Track Conference Sessions and Gala dinner or Cruise tour</li>
+      </ul>
+      
+      <p class="body">Registration for the workshop will be 121 EURs (VAT included) and separate from the main conferences. Workshop attendees will receive a certificate of participation. For registration, please visit: <a href="../registration" class="body-link">here</a>.</p>
+      
     <p class="bold">Poster Board Dimensions:</p>
       <p class="body">Authors presenting via poster boards are to be informed that poster boards are 130 cm height and 95 cm width.</p>
     </div>          
