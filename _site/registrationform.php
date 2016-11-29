4b7fa40abca2b8edf1d1bf42e02d9b9e99918ca0
Mansura Chowdhury <c.mansura@gmail.com>
2016-11-29 11:10:13 -0500

    Added the hotel form and updated the registration form

--- _site/registrationform.php
+++ _site/registrationform.php
@@ -43,7 +43,9 @@ $message = 'Dear Colleague,
 
 Thank you for registering for NDDTE 2017. If you have requested any official letters, please allow up to 5 business days to receive your documents.
 
-If you are an author, please make sure to send us your camera ready version and a signed copyright form via email to info@nddte.com. You can find the copyright form here: www.nddte.com/papers. Please note that failing to do so may result in an unsuccessful process of your registration.
+If you are an author, please make sure to send us the final version of your paper and a signed copyright form via email to info@nddte.com. You can find the copyright form here: www.nddte.com/papers. Please note that failing to do so may result in an unsuccessful process of your registration.
+
+To reserve a room with a DISCOUNTED price, please fill out the provided booking form here: www.awspt.com/accommodation/#Hotel_Alimara_Barcelona
 
 You can find your registration details below. If there are any errors in the information you have provided, please write an email to us at registration@nddte.com mentioning the correct information. Please note that you SHOULD NOT refill the form.
 
